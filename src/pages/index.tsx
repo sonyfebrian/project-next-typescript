60a9a7d17042b646251c32a36721379d7bed76fa
s <s@localhost.localdomain>
2023-11-24 14:37:21 +0700

    Add dashboard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 // import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import SidebarWithHeader from '@/components/Dashboard'
+import Dashboard from '@/components/Dashboard'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={` ${inter.className}`}>
-        <SidebarWithHeader />
+        <Dashboard > tes </Dashboard>
       </main>
     </>
   )
